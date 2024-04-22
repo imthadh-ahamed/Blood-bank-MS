d6bd926e97cf100790c5d5e8b5c3e69dad065a51
Imthadh Ahamed <143585845+imthadh-ahamed@users.noreply.github.com>
2024-04-23 00:49:40 +0530

    Create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Donordashboard from './pages/Donordashboard'
 
 function App() {
   return (
-    <div className='text-3xl text-red-600'>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/donordashboard" element={<Donordashboard />} />
+
+    </Routes>
+    </BrowserRouter>
   )
 }
 
