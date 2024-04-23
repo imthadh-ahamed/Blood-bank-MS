629d012284c1795d508c7a45121f30dace8daf54
Imthadh Ahamed <143585845+imthadh-ahamed@users.noreply.github.com>
2024-04-23 08:05:12 +0530

    Modify the Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
+// Importing neccessary components and packages
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Donordashboard from './pages/Donordashboard'
 
+// App function definition
 function App() {
   return (
+    // Wrapping the entire application with BrowserRouter to enable routing
     <BrowserRouter>
+    {/* Defining routes for different pages */}
     <Routes>
       <Route path="/" element={<Signin />} />
       <Route path="/signup" element={<Signup />} />
@@ -17,4 +21,5 @@ function App() {
   )
 }
 
+// Exporting App
 export default App
