1ebe5ff37266befff44adac6e22487d106e8ff72
Imthadh Ahamed <143585845+imthadh-ahamed@users.noreply.github.com>
2024-05-04 20:49:02 +0530

    Add some style parts

--- client/src/pages/Adddonor.jsx
+++ client/src/pages/Adddonor.jsx
@@ -294,9 +294,46 @@ function Adddonor() {
             <div className="flex justify-center">
               <button
                 type="submit"
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                class="w-full h-40 flex items-center justify-center cursor-pointer"
               >
-                Create
+                <div class="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold shadow text-white transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-blue-500 group">
+                  <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-blue-500 group-hover:h-full"></span>
+                  <span class="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      fill="none"
+                      class="w-5 h-5 text-white"
+                    >
+                      <path
+                        d="M14 5l7 7m0 0l-7 7m7-7H3"
+                        stroke-width="2"
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                      ></path>
+                    </svg>
+                  </span>
+                  <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      fill="none"
+                      class="w-5 h-5 text-white"
+                    >
+                      <path
+                        d="M14 5l7 7m0 0l-7 7m7-7H3"
+                        stroke-width="2"
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                      ></path>
+                    </svg>
+                  </span>
+                  <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+                    Create
+                  </span>
+                </div>
               </button>
             </div>
           </form>
